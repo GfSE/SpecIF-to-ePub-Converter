e6fb6fa898736eb7da06b05f5d43eb142985b1df
odungern <od@enso-managers.com>
2018-04-02 19:06:12 +0200

    setup

---
+++ source/js/toEpub.js
@@ -0,0 +1,23 @@
+function toEpub( specifData, opts ) {
+	"use strict";
+	// Accepts data-sets according to v0.10.4 or v0.11.2 and later.
+
+/*	// Check for missing options:
+	if ( !opts
+//		|| !opts.canvas
+		) return;
+//	if( !opts.index || opts.index>specifData.resources.length-1 ) opts.index = 0;
+*/		
+	// All required parameters are available, so we can begin:
+	var ePub = {
+		title: specifData.title,
+		cover: undefined,
+		content: '',
+		css: undefined,
+		mimetype: 'application/epub+zip'
+	};
+	
+	ePub.content = '<h1>Hello World</h1><p>Lorem ipsum ...</p>';
+		
+	return ePub
+}
